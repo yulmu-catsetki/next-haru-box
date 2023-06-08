0716dfdec47c2448fb3f7f1c6200f480bd76c150
YULMU <83626845+yulmu-catsetki@users.noreply.github.com>
2023-06-08 22:59:18 +0900

    add date/time

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { OrbitControls, ScrollControls } from "@react-three/drei";
 import { Room } from "./room.js";
 import { DiaryObjects } from './DiaryObjects.js';
 import { Outside } from './outside.js';
-import {Diary } from './Diary.js';
+import { Text } from '@react-three/drei';
+import { Diary } from './Diary.js';
 import * as THREE from 'three';
 import { useShadow } from '@react-three/drei';
 
-
-
 export function Experience({ diaries, handleDiaryClick, handleDashboardClick }) {
-  
+  const [currentDateTime, setCurrentDateTime] = useState('');
+
+  useEffect(() => {
+    const updateDateTime = () => {
+      const now = new Date();
+      const formattedDateTime = now.toLocaleString('ko-KR');
+      setCurrentDateTime(formattedDateTime);
+    };
+
+    const interval = setInterval(updateDateTime, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <group scale={[1, 1, 1]}>
-      <ambientLight intensity={0.5} />
+      <group position={[-0.7, 1.5, -0.7]} rotation={[0, Math.PI / 4, 0]}>
+        <Text
+          fontSize={0.2} // Adjust the font size of the text
+          color="white" // Adjust the color of the text
+        >
+          {currentDateTime}
+        </Text>
+      </group>
+    <group>
+    <ambientLight intensity={0.5} />
       <directionalLight
         position={[5, 10, 7]}
         intensity={1}
@@ -31,11 +54,12 @@ export function Experience({ diaries, handleDiaryClick, handleDashboardClick }) 
       <OrbitControls enableZoom={true} />
       <ScrollControls damping={1} style={{ overflow: 'hidden' }}>
         <Outside diaries={diaries} castShadow receiveShadow />
-        <Diary/>
+        <Diary />
         <DiaryObjects diaries={diaries} castShadow receiveShadow />
         <Room handleDashboardClick={handleDashboardClick} receiveShadow />
       </ScrollControls>
     </group>
+      
+    </group>
   );
 }
-
